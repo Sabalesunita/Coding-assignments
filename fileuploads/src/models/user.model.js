c40c1e6067251e380c51e614c5025bb3aa51fb31
Sabalesunita <97454504+Sabalesunita@users.noreply.github.com>
2022-03-26 23:32:21 +0530

    file uploads

---
+++ fileuploads/src/models/user.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    profilePic: [{ type: String, required: false }],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
